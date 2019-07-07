cd862128b2de87994707f26aee9fe310e9eff89e
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2019-07-06 21:45:44 -0700

    moved to C++11 folder

---
+++ sources/vector_implementation/C++11/test_vector.cxx
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "vector_implementation.h"
+
+int main(int argc, char **argv)
+{
+	Vector<int> vec;
+	/* test push back */
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+
+	for(int i = 0; i < vec.size(); ++i)
+		std::cout << vec[i] << std::endl;
+	/* end */
+	/* test resize, reserve, size and capacity functions */
+	std::cout << "vec size before : " << vec.size() << std::endl;
+	std::cout << "vec capacity before : " << vec.capacity() << std::endl;
+
+	vec.resize( 50 );
+	std::cout << "vec size after : " << vec.size() << std::endl;
+
+	vec.reserve( 100 );
+	std::cout << "vec capacity after : " << vec.capacity() << std::endl;
+	/* end */
+
+	/* test pop back */
+	std::cout << "vec size before : " << vec.size() << std::endl;
+	std::cout << "executes vec.pop_back() " << std::endl;
+	vec.pop_back();
+	std::cout << "vec size after : " << vec.size() << std ::endl;
+	/* end */
+
+	/* test iterator */
+	vec.resize(4);
+	vec.reserve(4);
+	std::cout << "vector size : " << vec.size() << std::endl;
+	std::cout << "vector capacity : " << vec.capacity() << std::endl;
+	std::cout << "test vector iterator " << std::endl;
+	Vector<int>::iterator it;
+	for(it = vec.begin(); it != vec.end(); ++it)
+		std::cout << *it << std::endl;
+	/* end */ 
+
+	/* test const_iterator */
+	std::cout << "test const iteator " << std::endl;
+	Vector<int>::const_iterator cit;
+	for(cit = vec.begin(); cit != vec.end(); ++cit)
+	       std::cout << *cit << std::endl;	
+	/* end */ 
+	return 0;
+}
